611a8c099c3816a15e144e8a5d0a15dd024f7e8e
Liam Mackie <liam.mackie@octopus.com>
2025-08-14 11:37:48 +1000

    initial

---
+++ api/v1beta1/workloadserviceaccount_types.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	v1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WorkloadServiceAccountSpec defines the desired state of WorkloadServiceAccount
+type WorkloadServiceAccountSpec struct {
+	// Scope defines the scope of the WorkloadServiceAccount.
+	// +required
+	Scope WorkloadServiceAccountScope `json:"scope"`
+	// Permissions defines the permissions for the WorkloadServiceAccount.
+	// +required
+	Permissions WorkloadServiceAccountPermissions `json:"permissions"`
+}
+
+// WorkloadServiceAccountStatus defines the observed state of WorkloadServiceAccount.
+type WorkloadServiceAccountStatus struct {
+	ServiceAccountName string `json:"serviceAccountName"`
+}
+
+// WorkloadServiceAccountScope defines the octopus scope for the WorkloadServiceAccount
+type WorkloadServiceAccountScope struct {
+	// +optional
+	Projects []string `json:"projects,omitempty"`
+	// +optional
+	Environments []string `json:"environments,omitempty"`
+	// +optional
+	Tenants []string `json:"tenants,omitempty"`
+	// +optional
+	Steps []string `json:"steps,omitempty"`
+}
+
+// WorkloadServiceAccountPermissions defines the permissions for the WorkloadServiceAccount.
+type WorkloadServiceAccountPermissions struct {
+	// +optional
+	ClusterRoles []v1.RoleRef `json:"clusterRoles,omitempty"`
+	// +optional
+	Roles []v1.RoleRef `json:"roles,omitempty"`
+
+	// Allows users to specify permissions, rather than roles
+	// A role will be created with the specified permissions
+	// +optional
+	Permissions []v1.PolicyRule `json:"permissions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// WorkloadServiceAccount is the Schema for the workloadserviceaccounts API
+type WorkloadServiceAccount struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty,omitzero"`
+
+	// spec defines the desired state of WorkloadServiceAccount
+	// +required
+	Spec WorkloadServiceAccountSpec `json:"spec"`
+
+	// status defines the observed state of WorkloadServiceAccount
+	// +optional
+	Status WorkloadServiceAccountStatus `json:"status,omitempty,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// WorkloadServiceAccountList contains a list of WorkloadServiceAccount
+type WorkloadServiceAccountList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []WorkloadServiceAccount `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&WorkloadServiceAccount{}, &WorkloadServiceAccountList{})
+}
