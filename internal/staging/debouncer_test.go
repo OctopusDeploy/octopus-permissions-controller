8387e51db4489ce770ca4ef21747b36313deed0c
Eddy Moulton <eddy.moulton@octopus.com>
2025-12-01 11:09:18 +1100

    test: use synctest

--- internal/staging/debouncer_test.go
+++ internal/staging/debouncer_test.go
@@ -3,43 +3,53 @@ package staging
 import (
 	"context"
 	"testing"
+	"testing/synctest"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 func TestDebouncer(t *testing.T) {
 	t.Run("callback is called only once during timeout period", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+		synctest.Test(t, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-		mockObj := NewMockedCallbackObj()
-		debouncer := NewDebouncer(100*time.Millisecond, mockObj.Callback)
-		debouncer.Start(ctx)
+			mockObj := NewMockedCallbackObj()
+			debouncer := NewDebouncer(100*time.Millisecond, mockObj.Callback)
+			debouncer.Start(ctx)
 
-		for range 5 {
-			debouncer.Debounce()
-			time.Sleep(20 * time.Millisecond)
-		}
+			for range 5 {
+				debouncer.Debounce()
+				time.Sleep(20 * time.Millisecond)
+			}
 
-		assert.Eventually(t, func() bool { return mockObj.AssertNumberOfCalls(t, "Callback", 1) }, 1000*time.Millisecond, 100*time.Millisecond)
+			time.Sleep(1 * time.Second)
+			synctest.Wait()
+
+			mockObj.AssertNumberOfCalls(t, "Callback", 1)
+		})
 	})
 
 	t.Run("multiple callbacks called when requests sent over timeout", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+		synctest.Test(t, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-		mockObj := NewMockedCallbackObj()
-		debouncer := NewDebouncer(100*time.Millisecond, mockObj.Callback)
-		debouncer.Start(ctx)
+			mockObj := NewMockedCallbackObj()
+			debouncer := NewDebouncer(100*time.Millisecond, mockObj.Callback)
+			debouncer.Start(ctx)
 
-		for range 10 {
-			debouncer.Debounce()
-			time.Sleep(20 * time.Millisecond)
-		}
+			for range 10 {
+				debouncer.Debounce()
+				time.Sleep(20 * time.Millisecond)
+			}
 
-		assert.Eventually(t, func() bool { return mockObj.AssertNumberOfCalls(t, "Callback", 2) }, 1000*time.Millisecond, 100*time.Millisecond)
+			time.Sleep(1 * time.Second)
+			synctest.Wait()
+
+			mockObj.AssertNumberOfCalls(t, "Callback", 2)
+		})
 	})
 }
 
